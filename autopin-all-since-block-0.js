7b653d9992c8688dfe2b03cc0ba1721c8b2b6a39
MrFasolo97 <79fasolo97@gmail.com>
2021-08-29 15:45:18 +0200

    Added the autopinner since block 0

---
+++ autopin-all-since-block-0.js
@@ -0,0 +1,92 @@
+const syncClient = require('sync-rest-client');
+const { exec } = require('child_process');
+const MongoClient = require('mongodb').MongoClient;
+
+
+fetchAll();
+async function fetchAll() {
+  let numProcs = 0;
+  function remove1process() {
+    numProcs -= 1;
+  }
+  const filter = {
+    'ts': {
+      '$gte': 1601557480488
+    }
+  };
+  const mongoDBClient = new MongoClient('mongodb://10.147.17.3:27017/avalon?readPreference=primary&appname=MongoDB%20Compass&directConnection=true&ssl=false');
+  await mongoDBClient.connect();
+  const mongoDB = mongoDBClient.db("avalon");
+  const contents_collection = mongoDB.collection("contents");
+  var response = await contents_collection.find(filter).toArray();
+  var last_exec = 0;
+  var numRows = 0;
+  var last_error_print = 0;
+  var num_supressed_errors = 0;
+  if (numProcs < 50) {
+      if(typeof response != 'undefined') {
+        for (var document_var in response) {
+          if (Date.now() - last_exec >= 3000) {
+            last_exec = Date.now();
+            console.log("Wrote "+numRows+" rows...");
+          }
+          document_var = response[document_var];
+          if(typeof document_var.json != 'undefined' && typeof document_var.json.files != 'undefined') {
+            if(typeof document_var.json.nsfw == 'undefined') {
+              var nsfw = -1;
+            } else if(document_var.json.nsfw.toString().includes("false")) {
+              var nsfw = 0;
+            } else {
+              var nsfw = document_var.json.nsfw;
+            }
+            if(typeof document_var.json.oc == 'undefined') {
+              var oc = -1;
+            } else {
+              var oc = document_var.json.oc;
+            }
+          }
+          if(typeof document_var.json.files != 'undefined' &&
+          (typeof document_var.json.files.btfs != 'undefined'
+          || typeof document_var.json.files.ipfs != 'undefined')) {
+            if(typeof document_var.json.files.ipfs != 'undefined') {
+              for (var file in document_var.json.files.ipfs.vid) {
+                file = document_var.json.files.ipfs.vid[file];
+                numProcs += 1;
+                exec("ipfs-cluster-ctl pin add --expire-in "+24*30*18+"h "+file, remove1process);
+                console.log("IPFS: Pinned video: "+file);
+              }
+              for (var file in document_var.json.files.ipfs.img) {
+                file = document_var.json.files.ipfs.img[file];
+                numProcs += 1;
+                exec("ipfs-cluster-ctl pin add --expire-in "+24*30*18+"h "+file, remove1process);
+                console.log("IPFS: Pinned img: "+file);
+              }
+            }
+            if(typeof document_var.json.files.btfs != 'undefined') {
+              for (var file in document_var.json.files.btfs.vid) {
+                file = document_var.json.files.btfs.vid[file];
+                numProcs += 1;
+                exec("btfs pin add "+file, remove1process);
+                console.log("BTFS: Pinned video: "+file);
+              }
+              for (var file in document_var.json.files.btfs.img) {
+                file = document_var.json.files.btfs.img[file];
+                numProcs += 1;
+                exec("btfs pin add "+file, remove1process);
+                console.log("BTFS: Pinned img: "+file);
+              }
+            }
+          }
+      }
+    } else {
+      console.log(response.statusCode);
+    }
+  } else {
+    await sleep(1500);
+  }
+}
+function sleep(ms) {
+  return new Promise((resolve) => {
+    setTimeout(resolve, ms);
+  });
+}
